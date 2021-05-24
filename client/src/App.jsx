949f7b03ea1ff419c270bfff048bf38e38cf6316
Mihael Stormrage <mihan.msn@hotmail.com>
2021-05-24 10:04:20 +0700

    Migrate to bootstrap-table-next

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
 import { PureComponent } from 'react';
 import axios from 'axios';
 
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { CSVExport } from 'react-bootstrap-table2-toolkit';
+import filterFactory, { textFilter, selectFilter } from 'react-bootstrap-table2-filter';
+
+const { ExportCSVButton } = CSVExport;
 
 class App extends PureComponent {
   state = { employees: [], jobs: [], departments: [] };
@@ -17,39 +22,58 @@ class App extends PureComponent {
 
   render() {
     const { employees, jobs, departments } = this.state;
-    const textFilter = { type: 'TextFilter', condition: 'like' };
-    const selectFilter = (ent) => ({
-      type: 'SelectFilter',
-      condition: 'like',
-      options: ent.map(({ name }) => name),
-    });
+    const columns = [{
+      dataField: 'last_name',
+      text: 'Фамилия',
+      sort: true,
+      filter: textFilter(),
+    }, {
+      dataField: 'first_name',
+      text: 'Имя',
+      filter: textFilter(),
+    }, {
+      dataField: 'patronymic',
+      text: 'Отчество',
+      filter: textFilter(),
+    }, {
+      dataField: 'department',
+      text: 'Отдел',
+      filter: selectFilter({
+        options: departments.map(({ id, name }) => ({ [id]: name })),
+      }),
+    }, {
+      dataField: 'job',
+      text: 'Должность',
+      sort: true,
+      filter: textFilter(),
+    }, {
+      dataField: 'birthday',
+      text: 'Дата рождения',
+      type: 'date',
+    }, {
+      dataField: 'tel',
+      text: 'Мобильный телефон',
+    }, {
+      dataField: 'email',
+      text: 'Электронная почта',
+    }];
     return (
-      <BootstrapTable
-        data={employees}
-        keyField="id"
-        version="4"
-        hover
-        insertRow
-        deleteRow
-        selectRow={{ mode: 'checkbox' }}
-        exportCSV
-        pagination
-        ignoreSinglePage
-        options={{ }}
-      >
-        <TableHeaderColumn dataField="last_name" dataSort filter={textFilter}>Фамилия</TableHeaderColumn>
-        <TableHeaderColumn dataField="first_name" filter={textFilter}>Имя</TableHeaderColumn>
-        <TableHeaderColumn dataField="patronymic" filter={textFilter}>Отчество</TableHeaderColumn>
-        <TableHeaderColumn dataField="department" filter={selectFilter(departments)}>
-          Отдел
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="job" dataSort filter={selectFilter(jobs)}>
-          Должность
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="birthday">Дата рождения</TableHeaderColumn>
-        <TableHeaderColumn dataField="tel">Мобильный телефон</TableHeaderColumn>
-        <TableHeaderColumn dataField="email">Электронная почта</TableHeaderColumn>
-      </BootstrapTable>
+      <ToolkitProvider keyField="id" data={employees} columns={columns} bootstrap4 exportCSV>
+        {({ baseProps, csvProps }) => (
+          <div>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <ExportCSVButton {...csvProps} className="btn-success">Export CSV</ExportCSVButton>
+            <hr />
+            <BootstrapTable
+              /* eslint-disable-next-line react/jsx-props-no-spreading */
+              {...baseProps}
+              filter={filterFactory()}
+              noDataIndication="Table is Empty"
+              hover
+            />
+          </div>
+        )}
+      </ToolkitProvider>
     );
   }
 }
