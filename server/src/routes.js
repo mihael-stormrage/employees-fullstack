a6c95d3667fd491bd1e2ad13ab1a15f04167d420
Mihael Stormrage <mihan.msn@hotmail.com>
2021-05-28 11:25:43 +0700

    Implement POST employees transaction route

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,10 @@ const router = new Router();
 
 /* Employees */
 
+const getValues = ({ jobId, firstName, lastName, patronymic, tel, email, birthday }) => [
+  jobId, firstName, lastName, patronymic, tel, email, birthday
+];
+
 router.get('/employees', (req, res) => {
   const query = `
       SELECT e.id, first_name, last_name, patronymic, tel, email, birthday, j.name AS job, d.name AS department
@@ -13,15 +17,19 @@ router.get('/employees', (req, res) => {
   pool.query(query, (q_err, q_res) => res.json(q_res.rows));
 });
 
-router.post('/employees', (req, res, next) => {
-  const { jobId, firstName, lastName, patronymic, tel, email, birthday } = req.body;
-  const query = `
+router.post('/employees', async (req, res, next) => {
+  const upsertJobsQuery = `INSERT INTO department_jobs (department_id, name) VALUES ($1, $2) ON CONFLICT DO NOTHING`;
+  const addEmployeeQuery = `
     INSERT INTO employees(job_id, first_name, last_name, patronymic, tel, email, birthday)
-        VALUES (${jobId}, ${firstName}, ${lastName}, ${patronymic}, ${tel}, ${email}, ${birthday})`;
-  pool.query(query, (q_err, q_res) => {
-    if (q_err) next(q_err);
-    res.json(q_res.rows);
-  });
+        VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+  const { job, departmentId } = req.body;
+  const client = pool.connect();
+  await client.query('BEGIN');
+  const insertRes = await client.query(upsertJobsQuery, [departmentId, job]);
+  await client.query(addEmployeeQuery, getValues(req.body));
+  await client.query('COMMIT');
+  client.release();
+  res.json(insertRes.rows[0]);
 });
 
 router.put('/employees/:id', (req, res, next) => {
