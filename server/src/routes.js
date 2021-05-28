a5dd3eedb907b232710b7bf8815fe7c35312b83c
Mihael Stormrage <mihan.msn@hotmail.com>
2021-05-28 13:43:11 +0700

    Implement API CRUD

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,56 +4,58 @@ const router = new Router();
 
 /* Employees */
 
-const getValues = ({ jobId, firstName, lastName, patronymic, tel, email, birthday }) => [
-  jobId, firstName, lastName, patronymic, tel, email, birthday
+const getValues = ({ id, job_id, first_name, last_name, patronymic, tel, email, birthday }) => [
+  job_id, first_name, last_name, patronymic, tel, email, birthday, id
 ];
+
+const upsertEmployee = async (employeeQuery, req, res) => {
+  const upsertJobsQuery = `
+    INSERT INTO department_jobs (department_id, name) VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING id`;
+  const { job, job_id, department_id } = req.body;
+  const client = await pool.connect();
+  await client.query('BEGIN');
+  if (!job_id && job) req.body.job_id = (await client.query(upsertJobsQuery, [department_id || null, job])).rows[0].id;
+  const insertRes = await client.query(employeeQuery, getValues(req.body));
+  await client.query('COMMIT');
+  client.release();
+  res.json(insertRes.rows[0]);
+};
 
 router.get('/employees', (req, res) => {
   const query = `
-      SELECT e.id, first_name, last_name, patronymic, tel, email, birthday, j.name AS job, d.name AS department
+      SELECT e.id, first_name, last_name, patronymic, tel, email, birthday,
+             job_id, department_id, j.name AS job, d.name AS department
       FROM employees e
                LEFT JOIN department_jobs j ON j.id = e.job_id
                LEFT JOIN departments d ON d.id = j.department_id`;
   pool.query(query, (q_err, q_res) => res.json(q_res.rows));
 });
 
-router.post('/employees', async (req, res, next) => {
-  const upsertJobsQuery = `INSERT INTO department_jobs (department_id, name) VALUES ($1, $2) ON CONFLICT DO NOTHING`;
+router.post('/employees', (req, res, next) => {
   const addEmployeeQuery = `
     INSERT INTO employees(job_id, first_name, last_name, patronymic, tel, email, birthday)
         VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-  const { job, departmentId } = req.body;
-  const client = pool.connect();
-  await client.query('BEGIN');
-  const insertRes = await client.query(upsertJobsQuery, [departmentId, job]);
-  await client.query(addEmployeeQuery, getValues(req.body));
-  await client.query('COMMIT');
-  client.release();
-  res.json(insertRes.rows[0]);
+  upsertEmployee(addEmployeeQuery, req, res);
 });
 
-router.put('/employees/:id', (req, res, next) => {
-  const { jobId, firstName, lastName, patronymic, tel, email, birthday } = req.body;
-  const query=`
+router.put('/employees', (req, res, next) => {
+  const updateEmployeeQuery=`
     UPDATE employees
-        SET job_id = ${jobId},
-            first_name = ${firstName},
-            last_name = ${lastName},
-            patronymic = ${patronymic},
-            tel = ${tel},
-            email = ${email},
-            birthday = ${birthday}
-        WHERE id = ${req.params.id}`;
-  pool.query(query, (q_err, q_res) => {
-    if (q_err) next(q_err);
-    res.json(q_res.rows);
-  });
+        SET job_id = $1,
+            first_name = $2,
+            last_name = $3,
+            patronymic = $4,
+            tel = $5,
+            email = $6,
+            birthday = $7
+        WHERE id = $8`;
+  upsertEmployee(updateEmployeeQuery, req, res);
 });
 
 router.delete('/employees/:id', (req, res, next) => {
   const { id } = req.params;
-  const query = `DELETE FROM employees WHERE id = ${id}`;
-  pool.query(query, (q_err, q_res) => {
+  const query = `DELETE FROM employees WHERE id = $1`;
+  pool.query(query, [id], (q_err, q_res) => {
     if (q_err) next(q_err);
     res.json(q_res.rows);
   });
