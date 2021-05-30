3360cc453db5da2686b1097650d90ac20bf90724
Mihael Stormrage <mihan.msn@hotmail.com>
2021-05-30 07:21:21 +0700

    Implement custom job field input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ import axios from 'axios';
 import ComboBoxInput from './components/ComboBoxInput';
 
 class App extends PureComponent {
-  state = { employees: [], jobs: [], departments: [] };
+  state = {
+    employees: [], jobs: [], departments: [], editable: {},
+  };
 
   async componentDidMount() {
     const { data: employees } = await axios.get('/api/employees');
@@ -14,8 +16,20 @@ class App extends PureComponent {
     this.setState({ employees, jobs, departments });
   }
 
+  onEdit = (idColName, nameColName) => ({ id, name }) => {
+    this.setState({ editable: { [idColName]: id, [nameColName]: name } });
+  };
+
+  onEditJob = (props) => (
+    // eslint-disable-next-line react/destructuring-assignment
+    <ComboBoxInput value={props.value} options={this.state.jobs} onEdit={this.onEdit('job_id', 'job')} />
+  );
+
   render() {
-    const { employees, jobs, departments } = this.state;
+    console.log('App rendered');
+    const {
+      employees, jobs, departments, editable,
+    } = this.state;
     return (
       <MaterialTable
         columns={[
@@ -28,11 +42,9 @@ class App extends PureComponent {
             lookup: departments.reduce((acc, { id, name }) => ({ ...acc, [id]: name }), {}),
           }, {
             title: 'Должность',
-            field: 'job',
+            field: 'job_id',
             lookup: jobs.reduce((acc, { id, name }) => ({ ...acc, [id]: name }), {}),
-            editComponent: (props) => (
-              <ComboBoxInput {...props} options={jobs} />
-            ),
+            editComponent: this.onEditJob,
           }, {
             title: 'Дата Рождения',
             field: 'birthday',
@@ -53,14 +65,14 @@ class App extends PureComponent {
         ]}
         data={employees}
         title="Энергозапас: Сотрудники"
-        options={{ exportButton: true, filtering: true, search: false }}
+        options={{
+          exportButton: true, filtering: true, search: false, pageSize: 10,
+        }}
         editable={{
           onRowAdd: async (newData) => {
-            // const { job, department } = newData;
-            // const jobId = jobs.find(({ name }) => name === job)?.id;
-            // const departmentId = departments.find(({ name }) => name === department)?.id;
-            await axios.post('/api/employees', { ...newData /* jobId, departmentId */ });
-            this.setState({ employees: [...employees, newData] });
+            const newRow = { ...newData, ...editable };
+            await axios.post('/api/employees', newRow);
+            this.setState({ employees: [...employees, newData], editable: [] });
           },
           onRowDelete: async (oldData) => {
             const { id: dbId } = oldData;
@@ -68,12 +80,14 @@ class App extends PureComponent {
             this.setState({ employees: employees.filter(({ id }) => id !== dbId) });
           },
           onRowUpdate: async (newData, oldData) => {
-            console.log(newData);
-            await axios.put('/api/employees', newData);
+            const newRow = { ...newData, ...editable };
+            console.log(editable);
+            console.log(newRow);
+            await axios.put('/api/employees', newRow);
             const index = oldData.tableData.id;
             const updatedData = [...employees];
-            updatedData[index] = newData;
-            this.setState({ employees: updatedData });
+            updatedData[index] = newRow;
+            this.setState({ employees: updatedData, editable: [] });
           },
         }}
       />
--- client/src/components/ComboBoxInput.jsx
+++ client/src/components/ComboBoxInput.jsx
@@ -1,13 +1,41 @@
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 
-const ComboBoxInput = ({ onChange, value, options }) => (
+const filter = createFilterOptions();
+
+const ComboBoxInput = ({
+  onEdit, onChange, value, options,
+}) => (
   <Autocomplete
+    value={options.find(({ id }) => id === value)}
+    defaultValue={{}}
     options={options}
     renderInput={(params) => <TextField {...params} />}
-    onChange={(e, newValue) => onChange(newValue)}
-    getOptionLabel={(option) => option.name}
-    value={value}
+    onChange={(e, newValue) => {
+      // onChange(newValue);
+      if (typeof newValue === 'string') onEdit({ name: newValue });
+      // Create a new value from the user input
+      else if (newValue?.inputValue) onEdit({ name: newValue.inputValue });
+      else onEdit(newValue ?? {});
+    }}
+    filterOptions={(fOptions, params) => {
+      const filtered = filter(fOptions, params);
+      // Suggest the creation of a new value
+      if (params.inputValue !== '') {
+        filtered.push({ inputValue: params.inputValue, name: `Add "${params.inputValue}"` });
+      }
+      return filtered;
+    }}
+    getOptionLabel={(option) => {
+      // Value selected with enter, right from the input
+      if (typeof option === 'string') return option;
+      // Add "xxx" option created dynamically
+      if (option.inputValue) return option.inputValue;
+      // Regular option
+      return option.name;
+    }}
+    renderOption={(option) => option.name}
+    selectOnFocus
     freeSolo
   />
 );
