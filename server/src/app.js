e5b4fd9b7910aa497bf8eac89a0dcecace2aaa92
Mihael Stormrage <mihan.msn@hotmail.com>
2021-11-27 08:46:03 +0700

    Add prod ENV config to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import Express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import path from 'path';
 
 import apiRouter from './routes';
 
@@ -11,6 +12,9 @@ app.use(Express.urlencoded({ extended: true }));
 app.use(Express.json());
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(Express.static(path.join('..', 'client', 'build')));
+}
 app.use('/api', apiRouter);
 
 export default app;
